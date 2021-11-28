ec9db2b77386039358d64a68e37aca929f72e31f
Anton Kuzmitsky <tat7106@gmail.com>
2021-11-28 16:11:06 +0600

    Добавляет изначальную сборку

---
+++ webpack.config.js
@@ -0,0 +1,44 @@
+const path = require(`path`);
+const isDev = process.env.NODE_ENV === 'development';
+const isProd = !isDev;
+
+module.exports = {
+    mode: 'development',
+    entry: `./src/index.tsx`,
+    output: {
+        filename: `bundle.js`,
+        path: path.join(__dirname, `public`),
+    },
+    devServer: {
+        static: {
+            directory: path.resolve(__dirname, './public'),
+        },
+        port: 8080,
+        hot: isDev,
+        open: true,
+        historyApiFallback: true,
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(scss|css)$/,
+                use: ['style-loader', 'css-loader', 'sass-loader'],
+            },
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: `babel-loader`,
+                },
+            },
+            {
+                test: /\.(tsx|ts)?$/,
+                loader: `ts-loader`,
+            },
+        ],
+    },
+    resolve: {
+        extensions: [`.ts`, `.tsx`, `.js`, `json`],
+    },
+    devtool: `source-map`,
+};
